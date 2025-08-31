a242025ce8e34c45fd3d15a287928b26be3c98b1
minhi0505 <mineehi0505@gmail.com>
2025-08-31 21:58:17 +0900

    4. NavLink 사용하기

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import MainNav from "../components/MainNav.jsx";
 
 const AboutPage = () => {
     return (
         <>
-            <MainNav />
             <h1>
                 About 페이지입니다.
             </h1>
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import MainNav from "../components/MainNav.jsx";
 
 const BlogPage = () => {
     return (
         <>
-            <MainNav />
             <h1>
                 Blog페이지입니다.
             </h1>
