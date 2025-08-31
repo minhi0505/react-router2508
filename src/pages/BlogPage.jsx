8a35fc8783e3e680ed4d56a9c4c9d9cce0ccc502
minhi0505 <mineehi0505@gmail.com>
2025-08-31 21:10:49 +0900

    2. Link 사용하기

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import MainNav from "../components/MainNav.jsx";
 
 const BlogPage = () => {
     return (
-        <h1>
-           Blog페이지입니다.
-        </h1>
+        <>
+            <MainNav />
+            <h1>
+                Blog페이지입니다.
+            </h1>
+        </>
+
     );
 };
 
